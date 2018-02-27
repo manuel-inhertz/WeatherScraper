4bdfc664f34651303a1e90de7ec830e238083cf9
Manuel <manuel@manuel-inhertz.com>
2018-02-27 09:40:18 +0100

    .

--- index.php
+++ index.php
@@ -56,7 +56,6 @@
 	<body>
 		<main>
 			<div class="container text-center mt-3">
-			<div id='MicrosoftTranslatorWidget' class='Dark' style='color:white;background-color:#555555'></div><script type='text/javascript'>setTimeout(function(){{var s=document.createElement('script');s.type='text/javascript';s.charset='UTF-8';s.src=((location && location.href && location.href.indexOf('https') == 0)?'https://ssl.microsofttranslator.com':'http://www.microsofttranslator.com')+'/ajax/v3/WidgetV3.ashx?siteData=ueOIGRSKkd965FeEGM5JtQ**&ctf=False&ui=true&settings=undefined&from=en';var p=document.getElementsByTagName('head')[0]||document.documentElement;p.insertBefore(s,p.firstChild); }},0);</script>
 				<header>
 					<h1 class="display-4">What's The Weather Like?</h1>
 					<p class="lead">Enter the name of the city.</p>
